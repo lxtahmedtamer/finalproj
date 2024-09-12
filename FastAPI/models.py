efa4b6f83a60f4fafb9a037d3a8b7f2fe3bcd6e5
lxtahmedtamer <Ahmed.Tamer@lxt.ai>
2024-09-12 11:01:17 +0300

    Update models.py

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,9 +1,24 @@
-from sqlalchemy import Column, Integer, String, Boolean
+# models.py
+
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
 
-class ToDoItem(Base):
-    __tablename__ = "todos"
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    
+    todos = relationship("ToDo", back_populates="owner")
 
+class ToDo(Base):
+    __tablename__ = 'todos'
+    
     id = Column(Integer, primary_key=True, index=True)
     text = Column(String, index=True)
     completed = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+    
+    owner = relationship("User", back_populates="todos")
